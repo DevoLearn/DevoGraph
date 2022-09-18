9a87d82029dda58a3cf0e174f91ffa45554c596c
DevoWorm Group <balicea@openworm.org>
2022-09-18 17:13:40 -0500

    Create install_dependencies.sh

---
+++ stage_1/install_dependencies.sh
@@ -0,0 +1,4 @@
+pip install dgl jupyter
+pip install -r devolearn/requirements.txt
+pip uninstall torch torchvision torchaudio
+conda install torchvision==0.8.0 torchaudio==0.7.0 pytorch==1.7.0 -c pytorch
